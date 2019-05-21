2ba2ade035a5fe4cb9cc8423c707d32cee231ca6
sgfs <gdfgs@dfgsgf>
2019-05-21 11:56:38 +0700

    Initial commit

---
+++ Polymorphism2/ConvertToJson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism2
+{
+    class ConvertToJson : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs)
+        {
+            string json = JsonConvert.SerializeObject(mhs);
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ Polymorphism2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.npm = "17.11.1201";
+            mhs.nama = "jhannad Nur Alfian";
+            mhs.email = "alfiarstudents.ariikom.ac.id";
+            Console.WriteLine("Pilih Format Konversi Data:");
+            Console.WriteLine("1.Json");
+            Console.WriteLine("2.XML");
+            Console.WriteLine("3.Csv");
+            Console.Write("Pilihan: ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+            IConvertObject convert; // dekiarasi objek konversi
+            if (nomorPrinter == 1)
+                convert = new ConvertToJson();
+            else if (nomorPrinter == 2)
+                convert = new ConvertToXml();
+            else
+                convert = new ConvertToCsv();
+            // koriversi objek mhs ber’dasarkan foreat yang dipilih
+            convert.Convert(mhs);
+            Console.ReadKey();
+        }
+    }
+}
